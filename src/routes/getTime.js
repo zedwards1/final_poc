2fdfeaab61cfea6f5223d4ed40f45394899d8c52
Zachary Edwards <zked222@uky.edu>
2023-11-23 12:39:26 -0500

    Hot Fix for time

--- src/routes/getTime.js
+++ src/routes/getTime.js
@@ -1,6 +1,10 @@
 const db = require('../persistence');
 
 module.exports = async (req, res) => {
-    const time = new Date().toLocaleTimeString();
-    res.send(time);
+    let time = new Date();
+    let offset = time.getHours() - 5;
+    time.setHours(offset);
+    let data =  {time: time.toLocaleString('en-us', {hour: '2-digit', minute: '2-digit', hour12: false}).replace(/:/g, '')};
+    res.send(JSON.stringify(data));
+    //res.send(time.toLocaleTimeString('en-us', {hour: '2-digit', minute: '2-digit'}).replace(/:/g, '').replace(/\D/g, ''));
 };
